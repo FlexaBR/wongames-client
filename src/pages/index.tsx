483f26b94f03c045e2541226dc01a76b46ee192f
Ricardo Gutheil <r.gutheil@terra.com.br>
2021-02-03 11:49:26 -0300

    back

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,15 +10,22 @@ export default function Index(props: HomeTemplateProps) {
 // ATENÇÃO:
 // os métodos getStaticProps/getServerSideProps SÓ FUNCIONAM EM PAGES
 
+// 3 tipos:
 // getStaticProps => gerar estático em build time
-// getServerSideProps => gerar via ssr a cada request
-// getInitialProps => gerar via ssr a cada request
-export function getServerSideProps() {
+// getServerSideProps => gerar via ssr (server side rendering) a cada request (nunca vai para o bunde do client (arquico JS menor))
+// getInitialProps => gerar via ssr a cada request (vai para o client, faz hydrate do lado do client depois do primeiro request)
+
+export async function getServerSideProps() {
+  // server side
+
   // faz lógica
   // pode ser buscar dados numa API
   // fazer calculo|leitura de context
 
+  // não se utiliza hooks aqui, hook é client side
+
   // retorno dos dados
+
   return {
     props: {
       banners: bannersMock,
